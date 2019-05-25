933b78f5487a3c51c99fbc732b151362b72f8611
Ken_G <gonzalk2095@gmail.com>
2019-05-25 12:11:55 -0400

    Test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scrape_WorldCup
+import Scrape_WorldCup
 
 app = Flask(__name__)
 
@@ -17,19 +17,20 @@ def index():
     WC = mongo.db.WC.find_one()
     return render_template("index.html", WC=WC)
 
-@app.route("/scrape18")
+@app.route("/scrape")
 def scrape():
     WC = mongo.db.WC
-    WC_data = scrape_WorldCup.scrape_WC18()
+    WC_data = Scrape_WorldCup.scrape()
+    # WC_data = Scrape_WorldCup.scrape_WC18()
     WC.update({}, WC_data, upsert=True)
     return "Scraping Successful!"
 
-@app.route("/scrape18")
-def scrape():
-    WC = mongo.db.WC
-    WC_data = scrape_WorldCup.scrape_WC14()
-    WC.update({}, WC_data, upsert=True)
-    return "Scraping Successful!"
+# @app.route("/scrape18")
+# def scrape():
+#     WC = mongo.db.WC
+#     WC_data = scrape_WorldCup.scrape_WC14()
+#     WC.update({}, WC_data, upsert=True)
+#     return "Scraping Successful!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
